d08a5025266cd1e25516692d205cdc898d15749a
dicky08 <dickyf147@gmail.com>
2020-10-10 13:56:26 +0700

    add mysql2

--- src/controller/pekerja/pekerjaController.js
+++ src/controller/pekerja/pekerjaController.js
@@ -1,9 +1,17 @@
 // Call Model
-// const {getAllModelEmploye} = require('../../model/pekerja/pekerjaModel')
-
-
+const {getAllModelEmploye} = require('../../model/pekerja/pekerjaModel')
+// Call Response
+const {success,failed,errorServer} = require('../../helper/response')
+const {sendEmailPekerja} = require('../../helper/sendEmail')
 module.exports = {
-  getAllControllerEmploye: () => {
+  getAllControllerEmploye: async () => {
+    // try {
+    //   const getEmploye = await getAllModelEmploye()
+    //   sendEmailPekerja(email)
+    //   success(res, getEmploye, 'Success get all data Employe')
 
+    // } catch (error) {
+      
+    // }
   }
 }
---
+++ src/helper/sendEmail.js
@@ -0,0 +1,12 @@
+const {JWTPEKERJA,JWTPEREKRUT,JWT_REGIS,EMAIL,PASSWORD} = require('../helper/env')
+const nodeMailer = require('nodemailer')
+const jwt = require('jsonwebtoken')
+
+module.exports = {
+  sendEmailPerekrut: (email) => {
+    
+  },
+  sendEmailPekerja: (email) => {
+
+  }
+}
